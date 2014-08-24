f01200dbba716a65479a717694563585d15f6307
da99 <i-hate-spam-1234567@mailinator.com>
2014-08-23 23:09:00 -0500

    Dev. checkpoint: Touchups.

--- specs/da99_rack_middleware.rb
+++ specs/da99_rack_middleware.rb
@@ -2,8 +2,7 @@
 describe "da99_rack_middleware" do
 
   it "runs" do
-    true.should == true
-    # Da99_Rack_Middleware.should.flunk "No tests written."
+    Da99_Rack_Middleware.should.flunk "No tests written."
   end
 
 end # === describe da99_rack_middleware ===
