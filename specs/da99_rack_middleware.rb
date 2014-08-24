fca5f45d3b7e8dab29dca11aec5cc14be0b75a3c
da99 <i-hate-spam-1234567@mailinator.com>
2014-08-24 00:50:17 -0500

    Extracted, refactored.

--- specs/da99_rack_middleware.rb
+++ specs/da99_rack_middleware.rb
@@ -1,7 +1,12 @@
 
+def get var, path
+  `bin/get -w "%{#{var}}" "http://localhost:#{ENV['PORT']}#{path}"`.strip
+end
+
 describe "da99_rack_middleware" do
 
   it "runs" do
+    get(:http_code, '/hello').should == 301
     Da99_Rack_Middleware.should.flunk "No tests written."
   end
 
