2d0dea3d053b4444a48ee532f39355517d84761c
da99 <i-hate-spam-1234567@mailinator.com>
2014-08-23 21:38:54 -0500

    Changed: file names for sorting.

--- lib/da99_rack_middleware.rb
+++ lib/da99_rack_middleware.rb
@@ -1,10 +1,37 @@
 
+
 class Da99_Rack_Middleware
 
-  class << self
-  end # === class self ===
+  dir   = File.expand_path(File.dir(__FILE__) + '/da99_rack_middleware')
+  files = Dir.glob(dir + '/*.rb').sort
+  Names = files.map { |file|
+    base = File.basename(file).sub('.rb', '')
+    require "da99_rack_middleware/#{base}"
+    base.split('_').shift.join('_').to_sym
+  }
 
-  def initialize
+  def initialize main_app
+    @app = Rack::Builder.new do
+
+      use Rack::ContentLength
+      use Rack::Session::Cookie, secret: SecureRandom.urlsafe_base64(nil, true)
+      use Rack::Protection
+
+      if ENV['IS_DEV']
+        use Rack::CommonLogger
+        use Rack::ShowExceptions
+      end
+
+      Names.each { |name|
+        use Object.const_get(name)
+      }
+
+      run main_app
+    end
+  end
+
+  def call env
+    @app.call env
   end
 
 end # === class Da99_Rack_Middleware ===
