ab14e28710e9700394ae040e772c25ee719c5993
da99 <i-hate-spam-1234567@mailinator.com>
2014-08-24 15:01:28 -0500

    Touchup.

--- specs/helpers/helpers.rb
+++ specs/helpers/helpers.rb
@@ -1,4 +1,17 @@
 
 require 'Bacon_Colored'
-require 'da99_rack_middleware'
 require 'pry'
+require 'da99_rack_middleware'
+
+def get var, path
+  url = "http://localhost:#{ENV['PORT']}#{path}"
+
+  case var
+  when :http_code
+    `bin/get -w "%{#{var}}" "#{url}"`.strip.to_i
+  when :output
+    `bin/get                "#{url}"`
+  else
+    fail "Unknown option: #{var.inspect}"
+  end
+end # === def get
