430670563d3fd88ef95ad81916464a962cf499d6
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-17 02:02:40 -0500

    Added: ensure SERVER_NAME and HTTP_HOST match

--- lib/da99_rack_middleware/0050_Ensure_Host.rb
+++ lib/da99_rack_middleware/0050_Ensure_Host.rb
@@ -3,19 +3,29 @@ class Da99_Rack_Middleware
 
   class Ensure_Host
 
+    SERVER_NAME = 'SERVER_NAME'
+    LOCALHOST   = /\A(localhost|127\.0\.0\.1)\z/
+    HTT_HOST    = 'HTTP_HOST'
+
     def initialize new_app
       @app = new_app
     end
 
     def call e
-      if Da99_Rack_Middleware::HOSTS.include?(e['SERVER_NAME']) ||
-        ( 
-         Da99_Rack_Middleware::HOSTS.include?(:localhost) &&
-         e['SERVER_NAME'][/\A(localhost|127\.0\.0\.1)\z/]
-        )
-        return @app.call(e)
+      hosts    = Da99_Rack_Middleware::HOSTS
+      name     = e[SERVER_NAME]
+      host     = e[HTT_HOST]
+
+      is_valid = hosts.include?(name)
+
+      if !is_valid
+        is_local = hosts.include?(:localhost) && name[LOCALHOST]
+        is_valid = is_local
       end
 
+      is_match = host[/\A#{name}(:\d+)?\z/]
+
+      return @app.call(e) if is_valid && is_match
       Da99_Rack_Middleware.response 444, :text, 'Unknown error.'
     end
 
